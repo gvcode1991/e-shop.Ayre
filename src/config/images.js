b7dbd729ba780537bb2504508e8099dd1a1cf51c
Gabriel Villamayor <gabitokun@outlook.com>
2026-07-01 19:45:52 -0300

    correcion de rutas

---
+++ src/config/images.js
@@ -0,0 +1,11 @@
+export const images = {
+  logo: "https://res.cloudinary.com/dwifi7niu/image/upload/v1782940698/logo2_e9t5y4.png",
+  socialPreview: "https://res.cloudinary.com/dwifi7niu/image/upload/v1782940698/ayre_e9ggju.png",
+  heroDesktop: "https://res.cloudinary.com/dwifi7niu/image/upload/v1782931971/hero2_rw1udu.png",
+  heroMobile: "https://res.cloudinary.com/dwifi7niu/image/upload/v1782931971/hero2_rw1udu.png",
+  categories: {
+    camisetas: "https://res.cloudinary.com/dwifi7niu/image/upload/v1782932080/WhatsApp_Image_2026-06-28_at_8.23.40_PM_4_qflkyu.jpg",
+    conjuntos: "https://res.cloudinary.com/dwifi7niu/image/upload/v1782932144/WhatsApp_Image_2026-06-28_at_8.23.41_PM_2_fqbbbs.jpg",
+    selecciones: "https://res.cloudinary.com/dwifi7niu/image/upload/v1782932078/WhatsApp_Image_2026-06-28_at_8.23.40_PM_1_saxssz.jpg",
+  },
+};
